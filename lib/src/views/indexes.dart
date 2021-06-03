e896360b5d58f25a34b822f1bccded059feb245d
Charles <charleslanop@gmail.com>
2021-06-03 18:57:37 -0300

    feat(indexes) implementation

--- lib/src/views/indexes.dart
+++ lib/src/views/indexes.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_covid19_final/src/components/menu.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class Indexes extends StatefulWidget {
   const Indexes({Key? key}) : super(key: key);
@@ -11,6 +16,179 @@ class Indexes extends StatefulWidget {
 class _IndexesState extends State<Indexes> {
   final height = AppBar().preferredSize.height;
   final title = 'Índices'.toUpperCase();
+  final url =
+      Uri.parse('https://disease.sh/v3/covid-19/historical/all?lastdays=90');
+  late List<FlSpot> dataCases = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  void _loadData() async {
+    try {
+      final response = await http.get(url);
+      final listCases = json.decode(response.body);
+
+      Map<String, dynamic> data = listCases['cases'];
+      int count = 0;
+      // NumberFormat formatter = NumberFormat('###,000');
+      // double.parse(formatter.format(v).replaceAll(',', '.'));
+
+      data.forEach((k, v) {
+        dataCases.add(FlSpot(count.toDouble(), v));
+        count = count + 1;
+      });
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  String _getLastMonth(int value) {
+    final String daysAgo =
+        DateFormat.M().format(DateTime.now().subtract(Duration(days: value)));
+
+    switch (daysAgo) {
+      case '1':
+        return 'Janeiro';
+      case '2':
+        return 'Fevereiro';
+      case '3':
+        return 'Março';
+      case '4':
+        return 'Abril';
+      case '5':
+        return 'Maio';
+      case '6':
+        return 'Junho';
+      case '7':
+        return 'Julho';
+      case '8':
+        return 'Agosto';
+      case '9':
+        return 'Setemebro';
+      case '10':
+        return 'Outubro';
+      case '11':
+        return 'Novembro';
+      case '12':
+        return 'Dezembro';
+    }
+
+    return '';
+  }
+
+  LineChartData sampleData1() {
+    return LineChartData(
+      lineTouchData: LineTouchData(
+        touchTooltipData: LineTouchTooltipData(
+          tooltipBgColor: Colors.blueGrey.withOpacity(0.8),
+        ),
+        touchCallback: (LineTouchResponse touchResponse) {},
+        handleBuiltInTouches: true,
+      ),
+      gridData: FlGridData(
+        show: false,
+      ),
+      titlesData: FlTitlesData(
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 32,
+          getTextStyles: (value) => TextStyle(
+            color: Color(0xff72719b),
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+          margin: 10,
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 10:
+                return _getLastMonth(60);
+              case 45:
+                return _getLastMonth(30);
+              case 80:
+                return _getLastMonth(0);
+            }
+            return '';
+          },
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) => TextStyle(
+            color: Color(0xff75729e),
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 0:
+                return '0';
+              case 50000000:
+                return '50M';
+              case 100000000:
+                return '100M';
+              case 150000000:
+                return '150M';
+              case 200000000:
+                return '200M';
+            }
+            return '';
+          },
+          margin: 8,
+          reservedSize: 30,
+        ),
+      ),
+      borderData: FlBorderData(
+        show: true,
+        border: const Border(
+          bottom: BorderSide(
+            color: Color(0xff4e4965),
+            width: 4,
+          ),
+          left: BorderSide(
+            color: Colors.transparent,
+          ),
+          right: BorderSide(
+            color: Colors.transparent,
+          ),
+          top: BorderSide(
+            color: Colors.transparent,
+          ),
+        ),
+      ),
+      minX: 0,
+      maxX: 90,
+      maxY: 200000000,
+      minY: 0,
+      lineBarsData: linesBarData1(),
+    );
+  }
+
+  List<LineChartBarData> linesBarData1() {
+    final lineChartBarData1 = LineChartBarData(
+      spots: dataCases,
+      isCurved: true,
+      colors: [
+        const Color(0xff4af699),
+      ],
+      barWidth: 5,
+      isStrokeCapRound: true,
+      dotData: FlDotData(
+        show: false,
+      ),
+      belowBarData: BarAreaData(
+        show: false,
+      ),
+    );
+    return [
+      lineChartBarData1,
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,9 +217,96 @@ class _IndexesState extends State<Indexes> {
             option: '/indexes',
           ),
         ),
-        body: Center(
-          child: Text('Índices'),
-        ),
+        body: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: AspectRatio(
+                    aspectRatio: 1.23,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8.0),
+                        ),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xff2c274c),
+                            Color(0xff46426c),
+                          ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              SizedBox(
+                                height: 37,
+                              ),
+                              Text(
+                                'Dados da Covid 19',
+                                style: TextStyle(
+                                  color: Color(0xff827daa),
+                                  fontSize: 18,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                'Confirmados',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 2,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                'Global',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(
+                                height: 37,
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                    right: 16.0,
+                                    left: 6.0,
+                                  ),
+                                  child: LineChart(
+                                    sampleData1(),
+                                    swapAnimationDuration:
+                                        Duration(milliseconds: 250),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
       ),
     );
   }
