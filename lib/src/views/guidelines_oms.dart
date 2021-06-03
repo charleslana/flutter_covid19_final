e9651767c0c8b8bf848c1825153dfa77cc04598b
Charles <charleslanop@gmail.com>
2021-06-02 22:44:15 -0300

    feat(structure) update

--- lib/src/views/guidelines_oms.dart
+++ lib/src/views/guidelines_oms.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_covid19_final/src/components/menu.dart';
 
 class GuidelinesOms extends StatefulWidget {
   const GuidelinesOms({Key? key}) : super(key: key);
@@ -8,10 +9,31 @@ class GuidelinesOms extends StatefulWidget {
 }
 
 class _GuidelinesOmsState extends State<GuidelinesOms> {
+  final height = AppBar().preferredSize.height;
+  final title = 'Orientações OMS'.toUpperCase();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Olá'),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff37b8ae),
+          title: Text(title),
+          leading: IconButton(
+            icon: Image.asset('images/icons/guidelines_oms.png'),
+            onPressed: () => {},
+          ),
+        ),
+        bottomNavigationBar: SizedBox(
+          height: height,
+          child: Menu(
+            option: '/guidelines-oms',
+          ),
+        ),
+        body: Center(
+          child: Text('Orientações OMS'),
+        ),
+      ),
     );
   }
 }
--- lib/src/views/indexes.dart
+++ lib/src/views/indexes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_covid19_final/src/components/menu.dart';
 
 class Indexes extends StatefulWidget {
   const Indexes({Key? key}) : super(key: key);
@@ -8,10 +9,40 @@ class Indexes extends StatefulWidget {
 }
 
 class _IndexesState extends State<Indexes> {
+  final height = AppBar().preferredSize.height;
+  final title = 'Índices'.toUpperCase();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Olá'),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff37b8ae),
+          title: Text(title),
+          leading: IconButton(
+            icon: Image.asset('images/icons/indexes.png'),
+            onPressed: () => {},
+          ),
+          actions: [
+            IconButton(
+              icon: Image.asset('images/menu/search.png'),
+              tooltip: 'Pesquisar',
+              onPressed: () => {
+                print('appbar clicked'),
+              },
+            ),
+          ],
+        ),
+        bottomNavigationBar: SizedBox(
+          height: height,
+          child: Menu(
+            option: '/indexes',
+          ),
+        ),
+        body: Center(
+          child: Text('Índices'),
+        ),
+      ),
     );
   }
 }
